77199ffb5f931914961311a53ed64d31dc850bcf
Luca Gallinari <luke.gallinari@gmail.com>
2017-01-13 15:37:29 +0100

    Impl.s Open Graph for product and category pages

---
+++ src/app/code/community/Webgriffe/OpenGraph/Block/Opengraph.php
@@ -0,0 +1,22 @@
+<?php
+class Webgriffe_OpenGraph_Block_Opengraph extends Mage_Core_Block_Template
+{
+    const WEBGRIFFE_OPENGRAPH_GENERAL_APPLICATION_CONFIG_PATH = 'webgriffe_opengraph/general/application_id';
+    const WEBGRIFFE_OPENGRAPH_GENERAL_ADMIN_CONFIG_PATH = 'webgriffe_opengraph/general/admin_id';
+
+    /**
+     * @return mixed
+     */
+    public function getApplicationId()
+    {
+        return Mage::getStoreConfig(self::WEBGRIFFE_OPENGRAPH_GENERAL_APPLICATION_CONFIG_PATH);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAdminId()
+    {
+        return Mage::getStoreConfig(self::WEBGRIFFE_OPENGRAPH_GENERAL_ADMIN_CONFIG_PATH);
+    }
+}
