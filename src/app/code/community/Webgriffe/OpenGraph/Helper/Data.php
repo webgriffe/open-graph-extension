0814b13e7c1f9af10645921ca82df02236a32e37
Luca Gallinari <luke.gallinari@gmail.com>
2017-01-19 10:05:46 +0100

    Improves homepage cms ogimage

--- src/app/code/community/Webgriffe/OpenGraph/Helper/Data.php
+++ src/app/code/community/Webgriffe/OpenGraph/Helper/Data.php
@@ -3,12 +3,23 @@
 class Webgriffe_OpenGraph_Helper_Data extends Mage_Core_Helper_Abstract
 {
     const WEBGRIFFE_OPENGRAPH_PRODUCT_ONLY_BASE_IMAGE_CONFIG_PATH = 'webgriffe_opengraph/product_view/only_base_image';
-    
+    const WEBGRIFFE_OPENGRAPH_HOMEPAGE_IMAGE_PATH_CONFIG_PATH = 'webgriffe_opengraph/homepage/image_path';
+
     /**
      * @return bool
      */
     public function useOnlyBaseImageForProduct()
     {
         return Mage::getStoreConfig(self::WEBGRIFFE_OPENGRAPH_PRODUCT_ONLY_BASE_IMAGE_CONFIG_PATH);
+    }
+
+    /**
+     * @return string
+     */
+    public function getHomepageOgImagePath()
+    {
+        return Mage::getDesign()->getSkinUrl(
+            Mage::getStoreConfig(self::WEBGRIFFE_OPENGRAPH_HOMEPAGE_IMAGE_PATH_CONFIG_PATH)
+        );
     }
 }
