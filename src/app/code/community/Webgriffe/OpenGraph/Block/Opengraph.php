e616ef0abc69baec189a164dbe5acbf33e714936
Luca Gallinari <luke.gallinari@gmail.com>
2017-09-15 12:38:44 +0200

    Product opengraph now uses product descr if short descr is "empty" + refactors helper usages

--- src/app/code/community/Webgriffe/OpenGraph/Block/Opengraph.php
+++ src/app/code/community/Webgriffe/OpenGraph/Block/Opengraph.php
@@ -5,6 +5,17 @@ class Webgriffe_OpenGraph_Block_Opengraph extends Mage_Core_Block_Template
     const WEBGRIFFE_OPENGRAPH_GENERAL_ADMIN_CONFIG_PATH = 'webgriffe_opengraph/general/admin_id';
 
     /**
+     * @var Webgriffe_OpenGraph_Helper_Data
+     */
+    private $helper;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->helper = Mage::helper('webgriffe_opengraph');
+    }
+
+    /**
      * @return string
      */
     public function getApplicationId()
@@ -18,5 +29,26 @@ class Webgriffe_OpenGraph_Block_Opengraph extends Mage_Core_Block_Template
     public function getAdminId()
     {
         return Mage::getStoreConfig(self::WEBGRIFFE_OPENGRAPH_GENERAL_ADMIN_CONFIG_PATH);
+    }
+
+    /**
+     * @param Mage_Catalog_Model_Product $product
+     * @return string
+     */
+    public function getProductDescription(Mage_Catalog_Model_Product $product)
+    {
+        $descr = $product->getShortDescription();
+        if (empty($descr) || $descr === '&nbsp;' || $descr === ' ') {
+            $descr = $product->getDescription();
+        }
+        return $this->helper->sanitizeString($descr);
+    }
+
+    /**
+     * @return Webgriffe_OpenGraph_Helper_Data
+     */
+    public function getOpenGraphHelper()
+    {
+        return $this->helper;
     }
 }
